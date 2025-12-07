cb4ab656b8072e3b18273d25f3ac6489a6ff74f3
ST3V3DG <rosycrystal024@gmail.com>
2025-12-07 11:04:37 +0100

    use vh and vw instead of dvh and dvw to avoid unwanted scroll behavior

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Header from "@/components/header";
-import HeroSection from "@/components/sections/hero";
 import PagePreloader from "@/components/page-preloader";
 import FeaturesSection from "@/components/sections/features";
+import HeroSection from "@/components/sections/hero";
 
 export default function Home() {
 	return (
 		<PagePreloader>
 			<Header />
-			<main className="overflow-x-hidden h-dvh w-dvw">
+			<main className="overflow-x-hidden h-vh w-vw">
 				<HeroSection />
 				<FeaturesSection />
 			</main>
